bfd2a53a907c56ea993d9382efb65a2802984e34
eggduzao <eggduzao@gmail.com>
2025-08-15 14:20:55 -0300

    Removed big files, refactoring code structure

--- tests/main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INPUT_FOLDER="./DAB_Training_Input/"
-OUTPUT_FOLDER="./DAB_Training_Output/"
-ROOT_NAME="DAB_Training_Input"
-
-Stainalyzer --severity 0.5 $INPUT_FOLDER $OUTPUT_FOLDER $ROOT_NAME
-
--- tests/real.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INPUT_FOLDER="/Users/egg/Projects/Stainalyzer/data/input/"
-OUTPUT_FOLDER="./DAB_Training_Output_Real/"
-ROOT_NAME="input"
-
-Stainalyzer --severity 0.5 $INPUT_FOLDER $OUTPUT_FOLDER $ROOT_NAME
-
